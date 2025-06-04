4de9e7fb686e35c65530f6edb0cffec435a9e33c
NadineRUIZ <nruizbaudouin@udesa.edu.ar>
2025-06-04 17:24:09 -0300

    hice detalle serie, pelicula y movie genere

---
+++ js/detalleserie.js
@@ -0,0 +1,37 @@
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let id = queryStringObj.get("id");
+console.log(id);
+
+let article = document.querySelector(".detalle");
+let URL = `https://api.themoviedb.org/3/tv/${id}?api_key=cd21534ccf3ef8b078f7ac273cdf32ca`
+
+fetch(URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(results) {
+    console.log(results);
+
+    let generos = ""
+    for (let i=0; i< results.genres.length; i++){
+    generos += `<a href="detallegenero.html?id=${results.genres[i].id}&nombre=${results.genres[i].name}"> ${results.genres[i].name}</a> `
+    }
+    article.innerHTML = `
+
+     <article>
+                 <img src="https://image.tmdb.org/t/p/w500/${results.poster_path}" alt="${results.title}">       
+                <h1>${results.name}</h1>
+                <ul>
+                   <li>${generos}</li>
+                    <li>Rating: ${results.vote_average}</li>
+                    <li>Release Date: ${results.release_date}</li>
+                    <li>Popularity: ${results.popularity}</li>  
+                </ul>
+                 <p>${results.overview}</p>
+            </article>
+    `
+})
+.catch(function(error){
+    console.log("el error es:" + error);
+});
---
+++ js/moviegenres.js
@@ -0,0 +1,23 @@
+let url = "https://api.themoviedb.org/3/genre/movie/list?api_key=cd21534ccf3ef8b078f7ac273cdf32ca";
+let ul= document.querySelector(".generos");
+
+
+fetch (url)
+.then(function(response){
+  return response.json();
+})
+.then(function(data){
+  console.log(data);
+
+  for(let i = 0; i <data.genres.length; i++){
+    ul.innerHTML += `
+   <li><a href="./detallegenero.html?id=${data.genres[i].id}&nombre=${data.genres[i].name}"> ${data.genres[i].name}</a></li>
+      `
+  }
+})
+
+  .catch(function(error){
+    console.log('El error es:'+ error);
+  })
+
+
